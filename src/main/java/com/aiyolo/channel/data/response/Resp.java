89cdaf8294afe1edc4239c35a708b7d26d1245f0
穆书伟 <james@igelian.com>
2018-01-17 15:15:25 +0800

    三个接口的测试类提交代码 因为不知道数据库表的对应关系，我以直接请求@RequestParam的形式提交json数据， 后期以对象映射或者HttpServletRequest的形式来请求，回复报文和你们通用的回复不太相同，后期更改一下

---
+++ src/main/java/com/aiyolo/channel/data/response/Resp.java
@@ -0,0 +1,76 @@
+package com.aiyolo.channel.data.response;
+
+/**
+ * @description 统一回复报文回复
+ * @date 2018年1月16日 下午15:52
+ */
+public class Resp {
+
+    /**
+     * 请求接口标识
+     */
+    private String action;
+    /**
+     * 错误码
+     */
+    private String error;
+    /**
+     * 回复内容
+     */
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+
+    public Resp(String action,String error){
+        this.action =action;
+        this.error =error;
+    }
+
+    public Resp(String action,String error,Object data){
+        this.action = action;
+        this.error =error;
+        this.data =data;
+    }
+
+    /**
+     * @description 绑定成功回复
+     * @param action
+     * @return
+     */
+    public static Resp ok(String action,Object data){
+        return new Resp(action,"",data);
+    }
+
+    /**
+     * @descriptin 失败回复
+     * @param action
+     * @return
+     */
+    public static Resp error(String action,Object data){
+        return new Resp(action,"120",data);
+    }
+
+}
--- src/main/java/com/aiyolo/controller/BoxController.java
+++ src/main/java/com/aiyolo/controller/BoxController.java
@@ -1,14 +1,39 @@
 package com.aiyolo.controller;
 
+import com.aiyolo.channel.data.response.Resp;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * @description 绑定盒子 解绑盒子 测试类 尚未完成逻辑处理
+ * @date 2017年1月17日 下午14:43
+ */
 @RestController
 @RequestMapping("/box")
 public class BoxController {
 
-    @RequestMapping("/bind")
-    public String bindBox(){
-        return "";
+    @RequestMapping(value = "/bind",method = RequestMethod.POST)
+    public Resp bindBox(
+            @RequestParam("action") String action,
+            @RequestParam("shopid") String shopid,
+            @RequestParam ("imeis[]") List<String> imeis
+    ){
+        return Resp.ok(action,"");
+
+    }
+
+
+    @RequestMapping(value = "/unbind",method= RequestMethod.POST)
+    public Resp unbindBox(
+            @RequestParam("action") String action,
+            @RequestParam("shopid") String shopid,
+            @RequestParam("imeis[]") List<String> imeis
+    ){
+        return Resp.ok(action,"");
     }
 }
+
