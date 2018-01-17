d3792b99fc680d23358404e6f47ef4eddfba68b7
jason <227022258@qq.com>
2018-01-17 10:27:01 +0800

    bug fix:多个网关对同一个设备进行devsta汇报，导致状态错误

--- src/main/java/com/aiyolo/channel/data/processor/GatewayDevstaProcessor.java
+++ src/main/java/com/aiyolo/channel/data/processor/GatewayDevstaProcessor.java
@@ -5,6 +5,7 @@ import com.aiyolo.entity.Checked;
 import com.aiyolo.entity.DeviceStatus;
 import com.aiyolo.entity.GatewayStatus;
 import com.aiyolo.repository.CheckedRepository;
+import com.aiyolo.repository.DeviceRepository;
 import com.aiyolo.repository.DeviceStatusRepository;
 import com.aiyolo.service.DeviceStatusService;
 import com.aiyolo.service.GatewayAlarmService;
@@ -28,16 +29,24 @@ public class GatewayDevstaProcessor extends Processor {
 
             DeviceStatusService deviceStatusService = (DeviceStatusService) SpringUtil.getBean("deviceStatusService");
 
+            DeviceRepository deviceRepository = (DeviceRepository) SpringUtil.getBean("deviceRepository");
             JSONArray deviceStatuses = messageBodyJson.getJSONArray("devs");
 
+            String glImei = messageBodyJson.getString("imei");
+            int mid = messageBodyJson.getInt("mid");
             for (int i = 0; i < deviceStatuses.size(); i++) {
                 JSONObject deviceStatus = deviceStatuses.getJSONObject(i);
 
+                String imei = deviceStatus.getString("imei");
+                if (!glImei.equals(deviceRepository.findFirstByImeiOrderByIdDesc(imei).getGlImei())){
+                    //不是所属网关汇报的状态忽略掉
+                    continue;
+                }
                 deviceStatusRepository.save(new DeviceStatus(
                         deviceStatus.getString("dev"),
-                        deviceStatus.getString("imei"),
-                        messageBodyJson.getString("imei"),
-                        messageBodyJson.getInt("mid"),
+                        imei,
+                        glImei,
+                        mid,
                         deviceStatus.getInt("online"),
                         deviceStatus.getInt("err"),
                         deviceStatus.getInt("rssi"),
@@ -50,12 +59,9 @@ public class GatewayDevstaProcessor extends Processor {
 
 
                 //-------------------------增加巡检---------------------------------
-//                String glimei = messageBodyJson.getString("imei");
-                int mid = messageBodyJson.getInt("mid");
                 if (deviceStatus.getInt("check") == 1) {
                     CheckedRepository checkedRepository = (CheckedRepository)
                             SpringUtil.getBean("checkedRepository");
-                    String imei = deviceStatus.getString("imei");
                     checkedRepository.save(new Checked(imei, mid));
                     deviceStatusService.pushChecked(imei, mid);
 
