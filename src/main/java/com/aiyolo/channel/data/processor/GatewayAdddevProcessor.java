e04eb4048cdb50871fdf878a985677f9677ee3e5
jason <227022258@qq.com>
2018-01-24 16:08:34 +0800

    修复设备不在线bug

--- src/main/java/com/aiyolo/channel/data/processor/GatewayAdddevProcessor.java
+++ src/main/java/com/aiyolo/channel/data/processor/GatewayAdddevProcessor.java
@@ -5,17 +5,23 @@ import com.aiyolo.constant.AppNoticeTypeConsts;
 import com.aiyolo.constant.DeviceOnlineStatusConsts;
 import com.aiyolo.constant.ProtocolFieldConsts;
 import com.aiyolo.entity.Device;
+import com.aiyolo.entity.DeviceStatus;
 import com.aiyolo.repository.DeviceRepository;
+import com.aiyolo.repository.DeviceStatusRepository;
 import com.aiyolo.service.DeviceStatusService;
 import com.aiyolo.service.GatewayStatusService;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import static com.aiyolo.constant.DeviceOnlineStatusConsts.ONLINE;
 import static com.aiyolo.constant.ProtocolFieldConsts.DEV;
 import static com.aiyolo.constant.ProtocolFieldConsts.DEVS;
 import static com.aiyolo.constant.ProtocolFieldConsts.IMEI;
+import static com.aiyolo.constant.ProtocolFieldConsts.MID;
 import static com.aiyolo.constant.ProtocolFieldConsts.PID;
 
 public class GatewayAdddevProcessor extends Processor {
@@ -27,14 +33,16 @@ public class GatewayAdddevProcessor extends Processor {
         try {
             init(message);
 
-            DeviceRepository deviceRepository = (DeviceRepository)
-                    SpringUtil.getBean("deviceRepository");
-            DeviceStatusService deviceStatusService = (DeviceStatusService)
-                    SpringUtil.getBean("deviceStatusService");
+            DeviceRepository deviceRepository = (DeviceRepository) SpringUtil.getBean("deviceRepository");
+            DeviceStatusService deviceStatusService = (DeviceStatusService) SpringUtil.getBean("deviceStatusService");
+
+
+            DeviceStatusRepository deviceStatusRepository = (DeviceStatusRepository) SpringUtil.getBean("deviceStatusRepository");
 
             JSONArray devs = messageBodyJson.getJSONArray(DEVS);
 
             String glImei = messageHeaderJson.getString(IMEI);
+            int mid = messageBodyJson.getInt(MID);
             for (int i = 0; i < devs.size(); i++) {
                 JSONObject dev = devs.getJSONObject(i);
 
@@ -51,12 +59,15 @@ public class GatewayAdddevProcessor extends Processor {
                         glImei);
                 deviceRepository.save(device);
 
-                deviceStatusService.pushDeviceStatus(device, AppNoticeTypeConsts.ADD,
-                        DeviceOnlineStatusConsts.ONLINE);
+
+                //强制在线
+                deviceStatusService.forceOnline(deviceStatusRepository, imei, device, glImei, mid);
+
+                // 推送设备状态变化
+                deviceStatusService.pushDeviceStatus(device, AppNoticeTypeConsts.ADD);
             }
 
-            GatewayStatusService gatewayStatusService = (GatewayStatusService)
-                    SpringUtil.getBean("gatewayStatusService");
+            GatewayStatusService gatewayStatusService = (GatewayStatusService) SpringUtil.getBean("gatewayStatusService");
             gatewayStatusService.pushGatewayStatus(glImei);
 
             // 写入文件待后续处理
